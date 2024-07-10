b3828a3fdf438b87bc1517b0bbd2ca4001236ca3
Ryan Lockhart <ryanlockhart106@gmail.com>
2024-07-09 17:54:35 -0700

    completes lab

--- test/indexTest.js
+++ test/indexTest.js
@@ -6,12 +6,19 @@ describe('shout(string)', function() {
     expect(shout('hello')).toEqual('HELLO');
   })
 })
+function shout(string) {
+  return string.toUpperCase();
+}
 
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello');
   })
 })
+function whisper(string) {
+  return string.toLowerCase()
+}
+
 
 describe('logShout(string)', function() {
   it('takes a string argument and logs it in all caps using console.log()', function() {
@@ -25,6 +32,10 @@ describe('logShout(string)', function() {
   })
 })
 
+function logShout(string) {
+console.log(string.toUpperCase())
+}
+
 describe('logWhisper(string)', function() {
   it('takes a string argument and logs it in all lowercase using console.log()', function() {
     const spy = expect.spyOn(console, 'log').andCallThrough();
@@ -36,6 +47,9 @@ describe('logWhisper(string)', function() {
     console.log.restore();
   })
 })
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+  }
 
 describe('sayHiToHeadphonedRoommate(string)', function() {
   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
@@ -52,3 +66,15 @@ describe('sayHiToHeadphonedRoommate(string)', function() {
     );
   });
 })
+
+function sayHiToHeadphonedRoommate(string) {
+if(string===string.toLowerCase()){
+  return "I can\'t hear you!";
+}
+else if(string===string.toUpperCase()){
+  return "YES INDEED!"
+}
+else if(string==="Let\'s have dinner together!"){
+  return "I would love to!"
+}
+}
